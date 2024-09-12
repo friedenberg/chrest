6505d4a7f8b2a3c996745027de6fa23a743885a3
Sasha F <cornflake.matchbox397@icantbelieveitsnotgmail.com>
2024-09-12 07:33:17 -0400

    update

--- go/src/charlie/browser_items/items.go
+++ go/src/charlie/browser_items/items.go
@@ -5,31 +5,14 @@ import (
 	"net/url"
 
 	"code.linenisgreat.com/chrest/go/src/bravo/config"
-	"code.linenisgreat.com/zit/go/zit/src/alfa/errors"
 )
 
 type Item struct {
-	Id         ItemId `json:"id"`
-	Url        string `json:"url"`
-	Date       string `json:"date"`
-	Title      string `json:"title"`
-	ExternalId string `json:"external_id"` // external to the browser, so for us, it's actually our id
-}
-
-func (i Item) GetUrl() (u *url.URL, err error) {
-	ur := i.Url
-
-	if ur == "" {
-		err = errors.Errorf("empty url")
-		return
-	}
-
-	if u, err = url.Parse(ur); err != nil {
-		err = errors.Wrap(err)
-		return
-	}
-
-	return
+	Id         ItemId  `json:"id"`
+	Url        url.URL `json:"url"`
+	Date       string  `json:"date"`
+	Title      string  `json:"title"`
+	ExternalId string  `json:"external_id"` // external to the browser, so for us, it's actually our id
 }
 
 type ItemId struct {
