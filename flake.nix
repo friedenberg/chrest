9498bd438fdae24197ce564e3a9a09bc18402d34
Sasha F <cornflake.matchbox397@icantbelieveitsnotgmail.com>
2026-03-04 08:30:22 -0500

    fix: remove unused nixpkgs-master input from top-level flake

--- flake.nix
+++ flake.nix
@@ -1,7 +1,6 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/6d41bc27aaf7b6a3ba6b169db3bd5d6159cfaa47";
-    nixpkgs-master.url = "github:NixOS/nixpkgs/5b7e21f22978c4b740b3907f3251b470f466a9a2";
     utils.url = "https://flakehub.com/f/numtide/flake-utils/0.1.102";
 
     devenv-go.url = "github:amarbel-llc/purse-first?dir=devenvs/go";
@@ -12,7 +11,6 @@
     {
       self,
       nixpkgs,
-      nixpkgs-master,
       utils,
       devenv-go,
       devenv-browser_extension,
